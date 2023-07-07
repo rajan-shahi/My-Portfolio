386d2449a11751276e770ff5d1e52fdaf0f2949a
rajan <rajanshahi1267@gmail.com>
2023-07-06 21:51:02 -0700

    init code

---
+++ main.js
@@ -0,0 +1,16 @@
+ 
+//  var typed = new typed(".text", {
+// Strings: ["Frontend Developer","YouTube","Web Developer"],
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     backDelay: 1000,
+//     loop: true
+//  });
+
+var typed = new typed(".text",{
+    Strings: ["frontend Developer","Youtube","Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
